24858d96f818684583884facd93949f803d545c7
Nadeera Weerasinghe Arachchige <nadeera.weerasinghe@pearson.com>
2024-02-26 10:25:25 +1100

    a

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { HttpClient} from '@angular/common/http'
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,32 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  constructor(private _httpClient: HttpClient) { }
+
   title = 'ngwebsite2';
+
+  public authenticate(): void {
+    this.authentiateViaAPI().subscribe(res$ => {
+      console.log('frLogin: ', res$['body']);
+    });
+  }
+
+  private authentiateViaAPI():  Observable<any> {
+    console.log('inside authentiate');
+    let endpoint = 'https://nodejsapi1.vercel.app/authenticate';
+  
+    const httpOptions = {
+      headers: {
+        'Content-Type': 'application/json; charset=utf-8',
+        'Accept': 'application/json',
+      },
+      withCredentials: true,
+      observe: 'response' as 'body',
+    };
+    try {
+      return this._httpClient.post<any>(endpoint, JSON.stringify({}), httpOptions,);
+    } catch (err) {
+      console.error('There was an error while calling FR Page', err);
+    }
+  }
 }
